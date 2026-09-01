2cd68fe2bed698be7c56e53a1a913b4dbebd909c
Ornythorus <jpoitevin42@gmail.com>
2026-09-01 12:11:53 +0200

    feat: user (#4)

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
-
 $routes->get('login', [AuthController::class, 'loginView']);
 $routes->post('login', [AuthController::class, 'loginAction']);
 $routes->get('register', [AuthController::class, 'registerView']);
@@ -13,6 +12,14 @@ $routes->post('register', [AuthController::class, 'registerAction']);
 $routes->get('logout', [AuthController::class, 'logoutAction']);
 
 //Routes pour l'administration
-$routes->group('admin', ['namespace'=>'App\Controllers\Admin','filter' => 'group:admin'], function ($routes) {
-    $routes->get('/','AdminController::index');
+$routes->group('admin', ['namespace' => 'App\Controllers\Admin', 'filter' => 'group:admin'], function ($routes) {
+    $routes->get('/', 'AdminController::index');
+    $routes->group('user', function ($routes) {
+        $routes->get('/', 'UserController::index');
+        $routes->get('edit/(:num)', 'UserController::edit/$1');
+        $routes->get('new', 'UserController::new');
+        $routes->post('update', 'UserController::update');
+        $routes->post('create', 'UserController::create');
+    });
 });
+
